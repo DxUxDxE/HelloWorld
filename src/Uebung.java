dd80dd09143a21d7b80c93810afbc7e475712742
DxUxDxE <33025476+DxUxDxE@users.noreply.github.com>
2017-10-23 13:13:37 +0200

    Initial commit

---
+++ src/Uebung.java
@@ -0,0 +1,16 @@
+/**
+ * Diese Klasse gibt eine kurze Begrüßung auf
+ * dem Bildschirm aus.
+ */
+public class Uebung {
+   /**
+     * Gibt eine kurze Begrüßung auf dem Bildschirm aus.
+     *
+     * @param args  wird nicht verwendet
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+    }
+    
+}
